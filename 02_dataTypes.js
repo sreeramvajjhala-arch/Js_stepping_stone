0d65a1c3fae977a5929db00bfdaede1a5e4ad296
sreeramvajjhala-arch <sreeramvajjhala@gmail.com>
2025-09-29 16:22:50 +0530

    Add files via upload

---
+++ 02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as newer version
+
+
+//alert(3 + 3) // we can use alert, prompt, confirm in browser only
+
+
+console.log(3 + 3); // we can use console in browser and node js also
+console.log("Hello World") ; console.log(3 + 3)//code readability should be high this is not a good practice
+console.log("Sreeram")
+let name = "Sreeram"
+let age = 18
+let majorOrNot = true;
+console.log(typeof(age))
+
+
+/*
+    number => 2^53
+    bigint => larger than 2^53
+    string => "Sreeram" 'Sreeram' 
+    boolean => true/false
+    null => standalone value(empty or nothing) 
+        ** typeof null => object (bug)**
+    undefined => standalone value
+    symbol == unique mostly used in react js
+
+
+
+    object => array, function, regular expression
+    
+ */ 
